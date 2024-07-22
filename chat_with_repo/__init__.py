550afa5285212af90b9b7d1c86eea4876084f63a
lanarimarco <marco.lanari@smeup.com>
2024-07-22 23:01:07 +0200

    Add first working version

--- chat_with_repo/__init__.py
+++ chat_with_repo/__init__.py
@@ -8,8 +8,24 @@ GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+# Google settings
+# Client ID and secret are stored in the client_secret.json
+# Google project: https://console.cloud.google.com/apis/credentials?project=chatwithrepo-429720
+# Account: lanarimar@gmail.com
+CLIENT_SECRET_PATH = os.path.join(os.getcwd(), "client_secret.json")
+
+SCOPES = [
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "openid"
+]
+##########################################################################
+
 if not GITHUB_TOKEN:
-    raise Exception("GITHUB_TOKEN environment variable is not set.")
+    raise Exception("Wrong github settings. GITHUB_TOKEN environment variable is not set.")
 
 if not OPENAI_API_KEY:
-    raise Exception("OPENAI_API_KEY environment variable is not set.")
+    raise Exception("Wrong openai settings. OPENAI_API_KEY environment variable is not set.")
+
+if not os.path.exists(CLIENT_SECRET_PATH):
+    raise Exception(f"Wrong google auth settings. {CLIENT_SECRET_PATH} file not found.")
