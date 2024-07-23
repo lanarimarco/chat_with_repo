0b75e345ce42794260ceb3cac0a3a4a92f912dbc
lanarimarco <marco.lanari@smeup.com>
2024-07-23 22:57:48 +0200

    chore: Add environment variable check for AUTHORIZED_USERS

--- chat_with_repo/__init__.py
+++ chat_with_repo/__init__.py
@@ -12,21 +12,32 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # Client ID and secret are stored in the client_secret.json
 # Google project: https://console.cloud.google.com/apis/credentials?project=chatwithrepo-429720
 # Account: lanarimarco@gmail.com
-#Credenziali -> ID client OAuth 2.0 - development
+# Credenziali -> ID client OAuth 2.0 - development
 CLIENT_SECRET_PATH = os.path.join(os.getcwd(), "client_secret.json")
 
 SCOPES = [
     "https://www.googleapis.com/auth/userinfo.profile",
     "https://www.googleapis.com/auth/userinfo.email",
-    "openid"
+    "openid",
 ]
 ##########################################################################
 
+AUTHORAZED_USERS = os.getenv("AUTHORIZED_USERS")
+
 if not GITHUB_TOKEN:
-    raise Exception("Wrong github settings. GITHUB_TOKEN environment variable is not set.")
+    raise Exception(
+        "Wrong github settings. GITHUB_TOKEN environment variable is not set."
+    )
 
 if not OPENAI_API_KEY:
-    raise Exception("Wrong openai settings. OPENAI_API_KEY environment variable is not set.")
+    raise Exception(
+        "Wrong openai settings. OPENAI_API_KEY environment variable is not set."
+    )
 
 if not os.path.exists(CLIENT_SECRET_PATH):
     raise Exception(f"Wrong google auth settings. {CLIENT_SECRET_PATH} file not found.")
+
+if not AUTHORAZED_USERS:
+    raise Exception(
+        "Wrong authorized users settings. AUTHORIZED_USERS environment variable is not set."
+    )
